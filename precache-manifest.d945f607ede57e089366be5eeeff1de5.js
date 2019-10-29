bad25db0a067cfd262bfc70bf064668f0d8b58d0
Jeff Stamey <jeffstamey1016@gmail.com>
2019-10-29 10:24:27 -0400

    Updates

---
+++ precache-manifest.d945f607ede57e089366be5eeeff1de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcaca31db5cbd81c9545c0a4e19d894",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c10e314cfb90a91ad6b",
+    "url": "/monsters-rolodex/static/css/main.89f16835.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "0c10e314cfb90a91ad6b",
+    "url": "/monsters-rolodex/static/js/main.0971ce02.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
